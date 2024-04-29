76b67a685dfc2889cf71b72d5db3b2b227507e26
Harris Pyo <HPW9229@gmail.com>
2024-04-29 15:18:21 -0400

    Added header for each file and will add descriptions later

--- Book.py
+++ Book.py
@@ -1,3 +1,8 @@
+# "I pledge my honor that I have abided by the Stevens Honor System." - Shawn Aviles, Justin Ferber, Harris Pyo
+# Author: Shawn Aviles, Justin Ferber, Harris Pyo
+# Date: 5/5/24
+# Description: 
+
 from Media import Media
 
 class Book(Media):
---
+++ RecommenderGUI.py
@@ -0,0 +1,4 @@
+# "I pledge my honor that I have abided by the Stevens Honor System." - Shawn Aviles, Justin Ferber, Harris Pyo
+# Author: Shawn Aviles, Justin Ferber, Harris Pyo
+# Date: 5/5/24
+# Description: 
