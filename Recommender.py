c010cc3ab2ca373909bdd2a584a393158b395587
Harris Pyo <HPW9229@gmail.com>
2024-05-05 02:46:39 -0400

    Removed description

--- Recommender.py
+++ Recommender.py
@@ -1,7 +1,7 @@
 # "I pledge my honor that I have abided by the Stevens Honor System." - Shawn Aviles, Justin Ferber, Harris Pyo
 # Author: Shawn Aviles, Justin Ferber, Harris Pyo
 # Date: 5/5/24
-# Description: In this file 
+# Description:
 
 from Book import Book
 from Show import Show
